6e80a07183a5423803775358534aa8592e89be2d
Kevin Staunton-Lambert <kevstal@fb.com>
2022-07-29 12:42:40 +1000

    SSH

--- kjsl_start.sh
+++ kjsl_start.sh
@@ -8,7 +8,7 @@ docker build -t ubuntu:18.04 .
 
 run() {
 	sudo echo
-	sudo nohup docker run -it --rm -p $PORT:5901 -e USER=klambert ubuntu:18.04 bash -c 'cat /home/klambert/.vnc/xstartup && vncserver :1 -geometry 2880x1800 -depth 24 && tail -F /home/klambert/.vnc/*.log'
+	sudo nohup docker run -it --rm -p $PORT:5901 -p 630:22 --mount type=bind,source=/Users/klambert,target=/host -e USER=klambert ubuntu:18.04 bash -c 'cat /home/klambert/.vnc/xstartup && vncserver :1 -geometry 2880x1800 -depth 24 && tail -F /home/klambert/.vnc/*.log'
 	sudo disown
 	sudo tail -f nohup.out
 }
